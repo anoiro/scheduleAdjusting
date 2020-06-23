8cf1f9d1b957ada6f75c2ef0a41dd21dbb2084f3
anoiro <asano.kyoshiro.16@shizuoka.ac.jp>
2020-06-23 20:58:46 +0900

    'helper()のcalendar()をテスト'

--- app/helpers.php
+++ app/helpers.php
@@ -6,52 +6,54 @@ use Carbon\CarbonImmutable;
  * 一か月分の日付リストを返す
  */
 function dates(CarbonImmutable $month)
-    {
-        $dates = [];
-        
-        //カレンダーで先月の残り(7/28~7/31)を$date配列に入れる
-        //その月の1日を曜日を０〜６で出す
-        //$last = date('m', strtotime("first day of $month"));
-        $first=$month->firstOfMonth();
-        $last = date('w', strtotime("first day of $month"));
-        //dd($last);
+{
+    $dates = [];
 
-        for (; 0 <= $last-1; $last -= 1) {
-            $dates[] = new CarbonImmutable("$first-$last day");
-        }
-        //dd($dates);
+    //カレンダーで先月の残り(7/28~7/31)を$date配列に入れる
+    //その月の1日を曜日を０〜６で出す
+    //$last = date('m', strtotime("first day of $month"));
+    $first = $month->firstOfMonth();
+    $last = date('w', strtotime("first day of $month"));
+    //dd($last);
 
-        //今月分(8/1~8/31)を$date配列に入れる
-        $week = date('d', strtotime("last day of $month"));
-        //dd($week);
-        
-        for ($i = 0;  $i < $week; $i += 1) {
-            $dates[] = new CarbonImmutable("$first+$i day");
-        }
-        //dd($dates);
-
-        //カレンダーで来月の残り(今回9月分はなし)を$date配列に入れる
-        $end=$month->lastOfMonth();
-        $next = date('w', strtotime("last day of $month"));
-        for ($i = 1; $i <= (6 - $next); $i += 1) {
-            $dates[] = new CarbonImmutable("$end+$i day");
-        }
-        return $dates;
+    for (; 0 <= $last - 1; $last -= 1) {
+        $dates[] = new CarbonImmutable("$first-$last day");
     }
+    //dd($dates);
 
-    function calendar($month)
-    {
+    //今月分(8/1~8/31)を$date配列に入れる
+    $week = date('d', strtotime("last day of $month"));
+    //dd($week);
+
+    for ($i = 0; $i < $week; $i += 1) {
+        $dates[] = new CarbonImmutable("$first+$i day");
+    }
+    //dd($dates);
+
+    //カレンダーで来月の残り(今回9月分はなし)を$date配列に入れる
+    $end = $month->lastOfMonth();
+    $next = date('w', strtotime("last day of $month"));
+    for ($i = 1; $i <= (6 - $next); $i += 1) {
+        $dates[] = new CarbonImmutable("$end+$i day");
+    }
+    return $dates;
+}
+
+/**
+ * 一か月分の日付リストを一週間ごとにリスト化する
+ */
+function calendar(CarbonImmutable $month)
+{
+    $week = [];
+    //該当する月$monthのカレンダーで表示する用の配列を得る
+    $dates = dates($month);
+
+    for ($i = 0; $i < count($dates); $i += 7) {
         $week = [];
-        //該当する月$monthのカレンダーで表示する用の配列を得る
-        $dates = dates($month);
 
-        for ($i = 0; $i < count($dates); $i += 7) {
-            $week = [];
-
-            foreach (array_slice($dates, $i,7) as $date) {
-                $week[] = $date;
-            }
-            yield $week;
+        foreach (array_slice($dates, $i, 7) as $date) {
+            $week[] = $date;
         }
+        yield $week;
     }
-?>
+}
--- tests/Unit/HelperTest.php
+++ tests/Unit/HelperTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use Carbon\CarbonImmutable;
+use Generator;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -21,5 +22,25 @@ class HelperTest extends TestCase
         $this->assertCount(35, $actual);
         $this->assertEquals('2020-05-31', $actual[0]->format('Y-m-d'));
         $this->assertEquals('2020-07-04', $actual[34]->format('Y-m-d'));
+    }
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_calendar()
+    {
+        $input=CarbonImmutable::createFromFormat('Y-m-d', '2020-06-23');
+        $actual=calendar($input);
+
+        //マジック定数。文字列のクラス名を返す
+        $this->assertInstanceOf(Generator::class, $actual);
+
+        $array=iterator_to_array($actual);
+        $this->assertCount(5, $array);
+
+        foreach($array as $a){
+            $this->assertCount(7, $a);
+        }
     }
 }
