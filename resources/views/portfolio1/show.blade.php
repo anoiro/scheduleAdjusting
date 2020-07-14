9e6560ace449b27f312ffdfdfdb5bbc7aa427200
anoiro <asano.kyoshiro.16@shizuoka.ac.jp>
2020-07-15 01:55:04 +0900

    show.blade.phpの画像表示の部分を変えてテスト

--- resources/views/portfolio1/show.blade.php
+++ resources/views/portfolio1/show.blade.php
@@ -37,6 +37,7 @@
                     @if($img!=null)
                     <div>
                         <img src='data:img/jpg;base64,<?php print(base64_encode($img->img)); ?>' style="width: 50%; height: auto;" />
+                        <img src="data:image/jpg;base64,<?= $img->img ?>" style="width: 50%; height: auto;" />
                     </div>
                     @endif
 
